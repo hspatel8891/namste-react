9405b66f90c6c887908c5d9550ee1547fd8162b3
hspatel8891 <patel.harshad999@gmail.com>
2023-08-06 14:00:29 +0530

    Add files via upload
    Signed-off-by: hspatel8891 <patel.harshad999@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namste React using JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/011/468/885/original/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title></Title>
-    <h1 className="heading" tabIndex="5">
-      Namste React Functional Component
-    </h1>
-  </div>
-);
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card">
+      <img className="res-logo" alt="res-logo" src={props.image}></img>
+      <h3>{props.resName}</h3>
+      <h4>{props.cusine}</h4>
+      <h4>4.4 Star</h4>
+      <h4>30 min</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          resName="Dhiavik Foods"
+          cusine="North Indian"
+          image="https://images.herzindagi.info/image/2021/Jul/expensive-ffod-in-inida-main.jpg"
+          rating
+        />
+        <RestaurantCard
+          resName="Vandana Foods"
+          cusine="Fast Food"
+          image="https://www.eatthis.com/wp-content/uploads/sites/4/2022/06/fast-food-assortment-soda.jpg?quality=82&strip=1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+      <Body></Body>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
